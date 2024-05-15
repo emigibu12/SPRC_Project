7591a9820f3612752d24c02d779d70935896874a
emigibu12 <129791843+emigibu12@users.noreply.github.com>
2024-05-15 15:13:29 +0300

    PLAYERI
    miscarile playerilor

---
+++ bluecharacter.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class bluecharacter : Godot.CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -500.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle() * 1.2f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y += gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("move_up") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("move_left", "move_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed * 2;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
